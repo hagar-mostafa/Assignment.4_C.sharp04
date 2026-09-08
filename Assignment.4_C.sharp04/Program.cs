224e91937dba7d8b03edacbe104ebb9289e83ed9
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-08 14:26:23 +0200

    Solve Q2

--- Assignment.4_C.sharp04/Program.cs
+++ Assignment.4_C.sharp04/Program.cs
@@ -4,14 +4,16 @@ namespace Assignment._4_C.sharp04
 {
     public class Program
     {
-        #region Q1
-        class Book { private string password = "secret";  }
+        #region Q2
+        public class Book { 
+            internal int copiesInStock ; 
+        }
         public static void Main(string[] args)
         {
             Book book = new Book();
-           //Book.password // syntax error
-           // password property is private and cannot be accessed from outside the class
-            // The solution is to change the access modifier to public or internal, or to make a public method to access the property.
+           book.copiesInStock = 10; // This will work because the property is internal
+                                    // internal can be accessed within the same Project
+        Console.WriteLine(book.copiesInStock);
         }
         #endregion
     }
