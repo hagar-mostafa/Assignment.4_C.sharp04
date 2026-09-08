7fc53e880832d231cb2078dfd6e3df67483e5bd6
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-08 14:16:42 +0200

    Solve Q1

--- Assignment.4_C.sharp04/Program.cs
+++ Assignment.4_C.sharp04/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+#nullable disable
+namespace Assignment._4_C.sharp04
+{
+    public class Program
+    {
+        #region Q1
+        class Book { private string password = "secret";  }
+        public static void Main(string[] args)
+        {
+            Book book = new Book();
+           //Book.password // syntax error
+           // password property is private and cannot be accessed from outside the class
+            // The solution is to change the access modifier to public or internal, or to make a public method to access the property.
+        }
+        #endregion
+    }
+}
