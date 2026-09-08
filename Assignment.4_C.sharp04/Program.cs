099e876aaf5d4ecdd02e5b53d907361a9a5a39fa
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-08 15:08:11 +0200

    Solve Q5

--- Assignment.4_C.sharp04/Program.cs
+++ Assignment.4_C.sharp04/Program.cs
@@ -4,15 +4,17 @@ namespace Assignment._4_C.sharp04
 {
     public class Program
     {
-        #region Q4
+        #region Q5
         public class Book { 
             public Genre genre ; 
         }
         public static void Main(string[] args)
         {
             Book book = new Book();
-           book.genre = Genre.Science;
-            Console.WriteLine(book.genre); // Science
+         
+            Console.WriteLine($"The value of Fiction is: {(int)Genre.Fiction}"); // 0
+            Console.WriteLine($"The value of NonFiction is: {(int)Genre.NonFiction}"); // 1
+            Console.WriteLine($"The value of Science is: {(int)Genre.Science}"); // 2
         }
         #endregion
     }
